687be86c2f75158a224037e2c73a56a4dc16b8d5
Luiz Gustavo <gusstavopdm@gmail.com>
2023-01-20 19:02:25 -0300

    feat(HabitDay): implements logic according to percentage of completed habits

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -3,8 +3,16 @@ import {
   Dimensions,
   TouchableOpacityProps,
 } from 'react-native';
+import clsx from 'clsx';
 
-interface HabitDayProps extends TouchableOpacityProps {}
+import { generateProgressPercentage } from '../utils/generate-progress-percentage';
+import dayjs from 'dayjs';
+
+interface HabitDayProps extends TouchableOpacityProps {
+  date: Date;
+  amount?: number;
+  completed?: number;
+}
 
 const WEEK_DAYS = 7;
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
@@ -13,11 +21,33 @@ export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE =
   Dimensions.get('screen').width / WEEK_DAYS - (SCREEN_HORIZONTAL_PADDING + 5);
 
-export function HabitDay({ ...rest }: HabitDayProps) {
+export function HabitDay({
+  amount = 0,
+  completed = 0,
+  date,
+  ...rest
+}: HabitDayProps) {
+  const completedPercentage =
+    amount > 0 ? generateProgressPercentage(amount, completed) : 0;
+  const today = dayjs().startOf('day').toDate();
+  const isCurrentDay = dayjs(date).isSame(today);
+
   return (
     <TouchableOpacity
       activeOpacity={0.7}
-      className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800'
+      className={clsx('rounded-lg border-2 m-1', {
+        'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+        'bg-violet-900 border-violet-500':
+          completedPercentage > 0 && completedPercentage < 20,
+        'bg-violet-800 border-violet-500':
+          completedPercentage >= 20 && completedPercentage < 40,
+        'bg-violet-700 border-violet-500':
+          completedPercentage >= 40 && completedPercentage < 60,
+        'bg-violet-600 border-violet-500':
+          completedPercentage >= 60 && completedPercentage < 80,
+        'bg-violet-500 border-violet-400': completedPercentage >= 80,
+        'border-zinc-500 border-4': isCurrentDay,
+      })}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
       {...rest}
     />
